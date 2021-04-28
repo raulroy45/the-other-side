b831e455314a5b7bf6a13f88af2cacf0d9506c6d
Ritadhwaj <rroy21@uw.edu>
2021-04-28 19:10:20 +0530

    Changes to PlayerController.cs

--- The Other Side/Assets/Scripts/PlayerController.cs
+++ The Other Side/Assets/Scripts/PlayerController.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float moveSpeed;
-    public float jumpSpeed = 3;
+    public float moveSpeed = 6;
+    public float jumpSpeed = 12;
+    public float mass;
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     public bool isGrounded;
+    public Sprite Real_Bob;
+    public Sprite Wall_Bob;
     private Rigidbody2D rb2d;
     private Animator animator;
-    private bool isWallMerged = false;
+    private bool isWallMerged;
+    private Color Real_World_Color;
+    private Color Wall_World_Color;
 
     // Start is called before the first frame update
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        isWallMerged = false;
+        Real_World_Color = GetComponent<SpriteRenderer>().color;
+        Wall_World_Color = new Color(0,0,0,1);
     }
 
     // Update is called once per frame
@@ -25,12 +33,14 @@ public class PlayerController : MonoBehaviour {
                                             groundCheckRadius, whatIsGround);
         
         // Wall merge
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKeyDown(KeyCode.J)) {
             isWallMerged = !isWallMerged;
             if (isWallMerged) {
-                gameObject.layer = LayerMask.NameToLayer("Real World");
-            } else {
                 gameObject.layer = LayerMask.NameToLayer("Wall World");
+                gameObject.GetComponent<SpriteRenderer>().color = Wall_World_Color;
+            } else {
+                gameObject.layer = LayerMask.NameToLayer("Real World");
+                gameObject.GetComponent<SpriteRenderer>().color = Real_World_Color;
             }
         }
         if (Input.GetAxisRaw("Horizontal") > 0f) {
@@ -50,5 +60,6 @@ public class PlayerController : MonoBehaviour {
         }
         animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         animator.SetBool("isGrounded", isGrounded);
+        animator.SetBool("isWallMerged", isWallMerged);
     }
 }
