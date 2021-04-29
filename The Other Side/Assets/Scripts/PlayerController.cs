e8fba7046d5f2a460421ee988dea90b8a2993ca9
LeoLiao-Agoy <leo211liao@gmail.com>
2021-04-28 23:58:17 -0700

    initial stage 3 + unstuck on wall

--- The Other Side/Assets/Scripts/PlayerController.cs
+++ The Other Side/Assets/Scripts/PlayerController.cs
@@ -42,16 +42,19 @@ public class PlayerController : MonoBehaviour {
                 gameObject.GetComponent<SpriteRenderer>().color = Real_World_Color;
             }
         }
+        ChangeSpeedWForce();
         if (Input.GetAxisRaw("Horizontal") > 0f) {
-            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            // move right
+            // rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
             // change local scale depending upon sprite's scale values
             transform.localScale = new Vector3(1.55f,2.22f,1f);
         } else if (Input.GetAxisRaw("Horizontal") < 0f) {
-            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+            // move left
+            // rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
             // change local scale depending upon sprite's scale values
             transform.localScale = new Vector3(-1.55f,2.22f,1f);
         } else {
-            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            // rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
 
         if (Input.GetButtonDown("Jump") && isGrounded) {
@@ -60,5 +63,29 @@ public class PlayerController : MonoBehaviour {
         animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         animator.SetBool("isGrounded", isGrounded);
         animator.SetBool("isWallMerged", isWallMerged);
+    }
+
+
+    // one way to control, does not get stuck on wall
+    // change Bob's speed using force
+    // dont not feel very actiony tho, dragging a bit
+    void ChangeSpeedWForce() {
+        if (Input.GetAxisRaw("Horizontal") > 0f) {
+            // move right
+            rb2d.AddForce(new Vector2(moveSpeed, 0));
+            // too fast
+            if (rb2d.velocity.x > moveSpeed) {
+                rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            }
+        } else if (Input.GetAxisRaw("Horizontal") < 0f) {
+            // move left
+            rb2d.AddForce(new Vector2(-moveSpeed, 0));
+            // too fast
+            if (rb2d.velocity.x < -moveSpeed) {
+                rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+            }
+        } else {
+            rb2d.velocity = new Vector2(rb2d.velocity.x / 1.09f, rb2d.velocity.y);
+        }
     }
 }
