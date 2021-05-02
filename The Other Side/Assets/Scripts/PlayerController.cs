41885cc68b9c57ae9419fe6292af29e2139a6a38
LeoLiao-Agoy <leo211liao@gmail.com>
2021-05-01 23:57:52 -0700

    pushed experiment files to branch

--- The Other Side/Assets/Scripts/PlayerController.cs
+++ The Other Side/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour {
     public float jumpSpeed = 12;
     public Transform groundCheck;
     public float groundCheckRadius = 0.1f;
+    public float wallCheckRadius = 0.5f;
     public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
     public bool isGrounded;
+    public bool isNextToWall;
     public Sprite Real_Bob;
     public Sprite Wall_Bob;
     private Rigidbody2D rb2d;
@@ -28,64 +31,97 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 
-                                            groundCheckRadius, whatIsGround);
-        
-        // Wall merge
+        // Wall merging?
         if (Input.GetKeyDown(KeyCode.J)) {
             isWallMerged = !isWallMerged;
             if (isWallMerged) {
                 gameObject.layer = LayerMask.NameToLayer("Wall World");
-                gameObject.GetComponent<SpriteRenderer>().color = Wall_World_Color;
             } else {
                 gameObject.layer = LayerMask.NameToLayer("Real World");
-                gameObject.GetComponent<SpriteRenderer>().color = Real_World_Color;
             }
         }
-        // ChangeSpeedWForce();
+        // environments
+        // isGrounded = Physics2D.OverlapCircle(groundCheck.position, 
+        //                                     groundCheckRadius, whatIsGround);
+        isGrounded = Mathf.Abs(rb2d.velocity.y) < 0.1f;  // peek of a jump == isGounded!!
+
+        LayerMask curr_layer = isWallMerged ? LayerMask.NameToLayer("Wall World") : LayerMask.NameToLayer("Real World");
+        isNextToWall = Physics2D.OverlapCircle(
+            groundCheck.position, 
+            wallCheckRadius, 
+            curr_layer);
+            // );
+        // isNextToWall = Physics2D.IsTouchingLayers(rb2d.GetComponent<Collider2D>(), curr_layer);
+
+        ChangeSpeedWForce();
+        updateBobsLook();
+
+        /*
         if (Input.GetAxisRaw("Horizontal") > 0f) {
             // move right
             rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
-            // change local scale depending upon sprite's scale values
-            transform.localScale = new Vector3(1.55f,2.22f,1f);
         } else if (Input.GetAxisRaw("Horizontal") < 0f) {
             // move left
             rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
-            // change local scale depending upon sprite's scale values
-            transform.localScale = new Vector3(-1.55f,2.22f,1f);
         } else {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
+        */
 
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
-        }
         animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         animator.SetBool("isGrounded", isGrounded);
         animator.SetBool("isWallMerged", isWallMerged);
     }
 
 
+    void updateBobsLook() {
+        // change local scale depending upon sprite's scale values
+        if (Input.GetAxisRaw("Horizontal") > 0f) {  // moving right
+            transform.localScale = new Vector3(1.55f,2.22f,1f);
+        } else if (Input.GetAxisRaw("Horizontal") < 0f) {   // moving left
+            transform.localScale = new Vector3(-1.55f,2.22f,1f);
+        }
+        // wall merged look
+        if (isWallMerged) {
+            gameObject.GetComponent<SpriteRenderer>().color = Wall_World_Color;
+        } else {
+            gameObject.GetComponent<SpriteRenderer>().color = Real_World_Color;
+        }
+    }
+
     // one way to control, does not get stuck on wall
     // change Bob's speed using force
     // dont not feel very actiony tho, dragging a bit
-    // void ChangeSpeedWForce() {
-    //     if (Input.GetAxisRaw("Horizontal") > 0f) {
-    //         // move right
-    //         rb2d.AddForce(new Vector2(moveSpeed, 0));
-    //         // too fast
-    //         if (rb2d.velocity.x > moveSpeed) {
-    //             rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
-    //         }
-    //     } else if (Input.GetAxisRaw("Horizontal") < 0f) {
-    //         // move left
-    //         rb2d.AddForce(new Vector2(-moveSpeed, 0));
-    //         // too fast
-    //         if (rb2d.velocity.x < -moveSpeed) {
-    //             rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
-    //         }
-    //     } else {
-    //         rb2d.velocity = new Vector2(rb2d.velocity.x / 1.09f, rb2d.velocity.y);
-    //     }
-    // }
+    void ChangeSpeedWForce() {
+        // left or right
+        if (Input.GetAxisRaw("Horizontal") > 0f) {
+            // move right
+            rb2d.AddForce(new Vector2(moveSpeed, 0));
+            // too fast
+            if (rb2d.velocity.x > moveSpeed) {
+                rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            }
+        } else if (Input.GetAxisRaw("Horizontal") < 0f) {
+            // move left
+            rb2d.AddForce(new Vector2(-moveSpeed, 0));
+            // too fast
+            if (rb2d.velocity.x < -moveSpeed) {
+                rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+            }
+        } else {
+            rb2d.velocity = new Vector2(rb2d.velocity.x / 1.09f, rb2d.velocity.y);
+        }
+
+        // jump
+        if (Input.GetButtonDown("Jump")) {
+            if (isGrounded) {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+            } else if (isNextToWall) {
+                Debug.Log("wall jumping");
+                // wall jump
+                // flip direction!
+                rb2d.velocity = new Vector2(-rb2d.velocity.x, jumpSpeed);
+            }
+        }
+    }
 }
