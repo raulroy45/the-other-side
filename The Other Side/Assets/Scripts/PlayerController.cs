c52d5eb07412657abbc9c48db7f6f9a1cd1ac3b5
Ritadhwaj <rroy21@uw.edu>
2021-05-02 20:04:59 +0530

    Implemented Wall Jump

--- The Other Side/Assets/Scripts/PlayerController.cs
+++ The Other Side/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float moveSpeed = 6;
-    public float jumpSpeed = 12;
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.1f;
-    public LayerMask whatIsGround;
-    public bool isGrounded;
-    public Sprite Real_Bob;
-    public Sprite Wall_Bob;
     private Rigidbody2D rb2d;
     private Animator animator;
+    public float moveSpeed = 6;
+    public float jumpSpeed = 12;
+    public float groundCheckRadius = 0.1f;
+    public float wallCheckRadius = 0.3f;
     private bool isWallMerged;
+    public bool isGrounded;
+    public bool hasWallJumped;
+    public bool isAlongWall;
+    public bool isRight;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public LayerMask RW_Wall;
+    public LayerMask WW_Wall;
     private Color Real_World_Color;
     private Color Wall_World_Color;
 
@@ -22,47 +26,94 @@ public class PlayerController : MonoBehaviour {
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         isWallMerged = false;
+        isRight = true;
         Real_World_Color = GetComponent<SpriteRenderer>().color;
         Wall_World_Color = new Color(0,0,0,1);
     }
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetKeyDown(KeyCode.J)) {
+            handleWallMerging();
+        }
+        SetNearbyParameters();
+        HandleMovement();
+        if (Input.GetButtonDown("Jump")) {
+            HandleJump();
+        }
+        SetAnimParameters();
+        // ChangeSpeedWForce();
+    }
+
+    private void SetNearbyParameters() {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 
                                             groundCheckRadius, whatIsGround);
-        
-        // Wall merge
-        if (Input.GetKeyDown(KeyCode.J)) {
-            isWallMerged = !isWallMerged;
-            if (isWallMerged) {
-                gameObject.layer = LayerMask.NameToLayer("Wall World");
-                gameObject.GetComponent<SpriteRenderer>().color = Wall_World_Color;
-            } else {
-                gameObject.layer = LayerMask.NameToLayer("Real World");
-                gameObject.GetComponent<SpriteRenderer>().color = Real_World_Color;
-            }
+        if (isGrounded) {
+            hasWallJumped = false;
         }
-        // ChangeSpeedWForce();
-        if (Input.GetAxisRaw("Horizontal") > 0f) {
-            // move right
-            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
-            // change local scale depending upon sprite's scale values
-            transform.localScale = new Vector3(1.55f,2.22f,1f);
-        } else if (Input.GetAxisRaw("Horizontal") < 0f) {
-            // move left
-            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
-            // change local scale depending upon sprite's scale values
-            transform.localScale = new Vector3(-1.55f,2.22f,1f);
+        if (isWallMerged) {
+            isAlongWall = Physics2D.OverlapCircle(groundCheck.position,
+                                            wallCheckRadius, WW_Wall);
         } else {
-            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            isAlongWall = Physics2D.OverlapCircle(groundCheck.position,
+                                            wallCheckRadius, RW_Wall);            
         }
+    }
 
-        if (Input.GetButtonDown("Jump") && isGrounded) {
+    private void handleWallMerging() {
+        isWallMerged = !isWallMerged;
+        if (isWallMerged) {
+            gameObject.layer = LayerMask.NameToLayer("WW_Bob");
+            gameObject.GetComponent<SpriteRenderer>().color = Wall_World_Color;
+        } else {
+            gameObject.layer = LayerMask.NameToLayer("RW_Bob");
+            gameObject.GetComponent<SpriteRenderer>().color = Real_World_Color;
+        }
+}
+
+    private void HandleMovement() {
+        if (Input.GetAxisRaw("Horizontal") > 0f) {
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            if (!isRight) {
+                Flip();
+            }
+        } else if (Input.GetAxisRaw("Horizontal") < 0f) {
+            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+            if (isRight) {
+                Flip();
+            }
+        } else if (isGrounded) {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        } else if (isAlongWall && !hasWallJumped) {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y * 0.1f);
+        }
+    }
+
+    private void HandleJump() {
+        if (isGrounded && !isAlongWall) {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
         }
+        if (isAlongWall) {
+            hasWallJumped = true;
+            float jumpComp = jumpSpeed * (1f / Mathf.Sqrt(2));
+            rb2d.velocity = new Vector2(-jumpComp, jumpComp);
+            Flip();
+        }
+    }
+
+    private void SetAnimParameters() {
         animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         animator.SetBool("isGrounded", isGrounded);
         animator.SetBool("isWallMerged", isWallMerged);
+    }
+
+    private void Flip() {
+        isRight = !isRight;
+        if (isRight) {
+            transform.localScale = new Vector3(1.55f,2.22f,1f);
+        } else {
+            transform.localScale = new Vector3(-1.55f,2.22f,1f);
+        }
     }
 
 
