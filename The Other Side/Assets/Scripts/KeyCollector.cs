34df90c54faee15ba63c7854350917f013c9ca64
LeoLiao-Agoy <leo211liao@gmail.com>
2021-05-04 23:11:54 -0700

    lv 2

--- The Other Side/Assets/Scripts/KeyCollector.cs
+++ The Other Side/Assets/Scripts/KeyCollector.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class KeyCollector : MonoBehaviour
 {
+    // renderer of door
+    public SpriteRenderer targetRenderer;
+    // sprite of opened door
+    public Sprite targetSprite;
+    // TODO: support multiple keys
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Bob") {
             Destroy(gameObject);
             // open the door a bit?
+            targetRenderer.sprite = targetSprite;
         }
     }
 
