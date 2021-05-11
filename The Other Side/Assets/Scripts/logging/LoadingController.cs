4ec4c9ad86c9965cc19218dc88b85af9ff901e46
LeoLiao-Agoy <leo211liao@gmail.com>
2021-05-11 10:26:59 -0700

    add logging to tutorial lv

--- The Other Side/Assets/Scripts/logging/LoadingController.cs
+++ The Other Side/Assets/Scripts/logging/LoadingController.cs
@@ -14,7 +14,7 @@ public class LoadingController : MonoBehaviour
         string skey = "433bbc3ee8e5cfe4ded20a30b8ba1af8";
         int gameId = 202101;
         string gameName = "theotherside";
-        int cid = 1;
+        int cid = 511;  // may 11 testing
         CapstoneLogger logger = new CapstoneLogger(gameId, gameName, skey, 0);
 
         string userId = logger.GenerateUuid();
