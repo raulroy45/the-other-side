5d7ff8909811649a686f6e5576b5bc421cab242a
Ritadhwaj <rroy21@uw.edu>
2021-06-05 18:10:37 -0500

    intermediate change to lots of doors lvl

---
+++ The Other Side/Assets/Scripts/BoxDispenser.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDispenser : MonoBehaviour
+{
+    private bool dispense = false;
+    private bool killBox = false;
+    private bool hasPlayed = false;
+    private Vector3 startPos;
+    public GameObject box;
+
+    void Start() {
+        startPos = box.transform.position;
+    }
+
+    void Update() {
+        if (dispense) {
+            if (!killBox) {
+                box.GetComponent<SpriteRenderer>().enabled = true;
+                killBox = true;
+                box.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            } else {
+                if (!box.GetComponent<Animation>().isPlaying) {
+                    if (!hasPlayed) {
+                        box.GetComponent<Animation>().Play();
+                    } else {
+                        box.GetComponent<SpriteRenderer>().transform.position = startPos;
+                    }
+                    hasPlayed = !hasPlayed;
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        dispense = true;
+    }
+}
